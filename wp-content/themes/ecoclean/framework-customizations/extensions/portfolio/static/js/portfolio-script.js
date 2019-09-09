8059c6d5e75dc3f02a3269dbb79687fb22889c7a
Рустам <akhrorov.rustam@yandex.ru>
2019-09-09 16:42:53 +0300

    1
    1

--- wp-content/themes/ecoclean/framework-customizations/extensions/portfolio/static/js/portfolio-script.js
+++ wp-content/themes/ecoclean/framework-customizations/extensions/portfolio/static/js/portfolio-script.js
@@ -1,7 +1,26 @@
-"use strict";
-jQuery(document).ready(function ( $ ) {
-	$('#Container').mixItUp();
-	$('.wrapp-categories-portfolio .categories-item a').on('click', function (e) {
-		e.preventDefault();
+// "use strict";
+// jQuery(document).ready(function ( $ ) {
+// 	$('#Container').mixItUp();
+// 	// $('.wrapp-categories-portfolio .categories-item a').on('click', function (e) {
+// 	// 	e.preventDefault();
+// 	// });
+
+//     var mixer = mixitup(containerEl, {
+//     load: {
+//         sort: '.category_200'
+//     }
+//     });
+
+// });
+
+
+
+$(function(){
+	$('#Container').mixItUp({
+
+     load: {
+        filter: '.category_all'
+    }
+
 	});
 });
